c2184927b156f9d18cf3163cd643baf23f9e31c9
Adonis <adonis@localhost.localdomain>
2019-04-20 17:34:16 +0200

    ft_strmap.c

---
+++ ft_strmap.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: akalombo <akalombo@42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/04/18 10:11:04 by akalombo          #+#    #+#             */
+/*   Updated: 2019/04/18 10:50:35 by akalombo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char *           ft_strmap(char const *s, char (*f)(char))
+{
+    unsigned int count;
+    char* fresh;
+
+    fresh = ft_strnew(ft_strlen(fresh));
+    if(fresh == NULL)
+        return (NULL);
+    else
+    {
+        while (s[count] != '\0')
+        {
+            fresh[count] = f(s[count]);
+            count++;
+        }
+    }
+    return (fresh);
+}
